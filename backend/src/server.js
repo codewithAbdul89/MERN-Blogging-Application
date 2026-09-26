04c4a3dfef50f546e674424ece6d91f0bf41e149
Abdul Rehman <codewithabdul8@gmail.com>
2026-09-26 10:09:57 +0500

    Update crons

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,6 @@
 import "dotenv/config";
 import app from "./app.js";
 import connectDB from "./config/db.config.js";
-import "./cron/deleteHiddenComments.js";
 
 const PORT = process.env.PORT || 5000;
 
