e7d112a1305b9e1b7af1cfbff51ebba11e71306c
Abdul Rehman <codewithabdul8@gmail.com>
2026-09-26 21:02:56 +0500

    Update cookie options to use 'none' for sameSite and set secure to true

--- backend/src/services/cookie.options.js
+++ backend/src/services/cookie.options.js
@@ -1,14 +1,31 @@
+// export const accessTokenOptions = {
+//   sameSite: "lax",
+//   httpOnly: true,
+//   secure: process.env.NODE_ENV === "production",
+//   maxAge: 15 * 60 * 1000,
+// };
+
+// export const refreshTokenOptions = (rememberMe) => ({
+//   sameSite: "lax",
+//   httpOnly: true,
+//   secure: process.env.NODE_ENV === "production",
+
+//   ...(rememberMe && {
+//     maxAge: 7 * 24 * 60 * 60 * 1000,
+//   }),
+// });
+
 export const accessTokenOptions = {
-  sameSite: "lax",
+  sameSite: "none",
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
+  secure: true,
   maxAge: 15 * 60 * 1000,
 };
 
 export const refreshTokenOptions = (rememberMe) => ({
-  sameSite: "lax",
+  sameSite: "none",
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
+  secure: true,
 
   ...(rememberMe && {
     maxAge: 7 * 24 * 60 * 60 * 1000,
